b98c72f1855cb601d6739ad859ace0338766e85a
KushKim <ds12000@naver.com>
2017-02-20 02:51:05 +0900

    add-parser-and-mongoose

--- HelloWebServer/app.js
+++ HelloWebServer/app.js
@@ -1,9 +1,11 @@
+// import modules
 var express = require('express');
-
 var app = express();
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
-mongoose.connect("mongodb://<dbuser>:<dbpassword>@ds157529.mlab.com:57529/kushkim");
+// connect database
+mongoose.connect(process.env.MONGO_DB);
 var db = mongoose.connection;
 db.once("open",function() {
     console.log("DB Connected!");
@@ -12,7 +14,14 @@ db.on("error",function (err) {
     console.log("DB ERROR :", err);
 });
 
+// model setting
+
+// view setting
+
+// set middlewares
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+// start server
 app.listen(3000, function(){
     console.log('Server on!');
 });
