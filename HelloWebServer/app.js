935cc504cddcd0ffb29eb7b9a7017ec6294a43bb
KushKim <ds12000@naver.com>
2017-02-20 02:07:59 +0900

    addmanythings

--- HelloWebServer/app.js
+++ HelloWebServer/app.js
@@ -1,6 +1,16 @@
 var express = require('express');
 
 var app = express();
+var mongoose = require('mongoose');
+
+mongoose.connect("mongodb://<dbuser>:<dbpassword>@ds157529.mlab.com:57529/kushkim");
+var db = mongoose.connection;
+db.once("open",function() {
+    console.log("DB Connected!");
+});
+db.on("error",function (err) {
+    console.log("DB ERROR :", err);
+});
 
 app.use(express.static(__dirname + '/public'));
 app.listen(3000, function(){
