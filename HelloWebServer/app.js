b5021eccea78e2788684e769510b49296b3c5dc4
KushKim <ds12000@naver.com>
2017-02-21 14:26:38 +0900

    bug fixed

--- HelloWebServer/app.js
+++ HelloWebServer/app.js
@@ -1,16 +1,17 @@
 // import modules
 var express = require('express');
 var app = express();
+var path = require('path');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
 // connect database
 mongoose.connect(process.env.MONGO_DB);
 var db = mongoose.connection;
-db.once("open",function() {
+db.once("open", function () {
     console.log("DB연결성공!");
 });
-db.on("error",function (err) {
+db.on("error", function (err) {
     console.log("DB ERROR :", err);
 });
 
@@ -25,20 +26,25 @@ var Post = mongoose.model('post',postSchema);
 // view setting
 app.set("view engine", 'ejs')
 // set middlewares
+
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
+
 // set routes
-app.get('/posts', function(req,res){
-  Post.find({}, function (err,posts) {
-    if(err) return res.json({success:false, message:err});
-    res.json({success:true, data:posts});
-  });
+app.get('/posts', function (req, res){
+    Post.find({}).sort('-createdAt').exec(function (err, posts) {
+        if(err) return res.json({success:false, message:err});
+        res.render("posts/index", {data:posts});
+    });
+});
+
 }); // index
 app.post('/posts', function(req,res){
   Post.create(req.body.post,function (err,post) {
     if(err) return res.json({success:false, message:err});
     res.json({success:true, data:post});
   });
+    
 }); // create
 app.get('/posts/:id', function(req,res){
   Post.findById(req.params.id, function (err,post) {
